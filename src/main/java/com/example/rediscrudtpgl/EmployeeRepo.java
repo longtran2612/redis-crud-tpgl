920584d43fcaf021602cc0910adcd3bfb30fbb1d
Your Name <you@example.com>
2021-11-08 12:07:25 +0700

    crud list commit

--- src/main/java/com/example/rediscrudtpgl/EmployeeRepo.java
+++ src/main/java/com/example/rediscrudtpgl/EmployeeRepo.java
@@ -1,6 +1,7 @@
 package com.example.rediscrudtpgl;
 
 import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -11,30 +12,39 @@ import java.util.Set;
 @Repository
 public class EmployeeRepo {
     private HashOperations hashOperations;
+    private ListOperations listOperations;
+
 
     private RedisTemplate redisTemplate;
 
     public EmployeeRepo(RedisTemplate redisTemplate) {
 
-        this.hashOperations = redisTemplate.opsForHash();
-
+        //this.hashOperations = redisTemplate.opsForHash();
+        this.listOperations = redisTemplate.opsForList();
+        this.redisTemplate = redisTemplate;
     }
 
     public void saveEmployee(Employee employee) {
 
-        hashOperations.put("EMPLOYEE", employee.getId(), employee);
-
+       // hashOperations.put("EMPLOYEE", employee.getId(), employee);
+		listOperations.rightPush("EMPLOYEE", employee);
     }
 
     public List<Employee> findAll() {
 
-        return hashOperations.values("EMPLOYEE");
+        //return hashOperations.values("EMPLOYEE");
+        return listOperations.range("EMPLOYEE", 0, listOperations.size("EMPLOYEE"));
 
     }
 
     public Employee findById(Integer id) {
-        return (Employee) hashOperations.get("EMPLOYEE", id);
-
+       // return (Employee) hashOperations.get("EMPLOYEE", id);
+        List<Employee> employees = listOperations.range("EMPLOYEE", 0, listOperations.size("EMPLOYEE"));
+        for (Employee employee : employees) {
+            if(employee.getId() == id)
+                return employee;
+        }
+        return null;
     }
 
     public void update(Employee employee) {
@@ -42,7 +52,7 @@ public class EmployeeRepo {
     }
 
     public void delete(Integer id) {
-        hashOperations.delete("EMPLOYEE", id);
-
+        //hashOperations.delete("EMPLOYEE", id);
+        listOperations.rightPopAndLeftPush("EMPLOYEE", id);
     }
 }
